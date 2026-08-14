9d299893774b328999f2d36156f3156dd06f9449
Amos-developer <108217876+Amos-developer@users.noreply.github.com>
2026-08-14 19:08:14 +0300

    unify members savings and loans page design

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./assets/management-pages.css";
 
 const app = createApp(App);
 
